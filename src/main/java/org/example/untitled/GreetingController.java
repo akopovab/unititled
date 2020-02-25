1c7fe4025e6b6c219b125aa61146f3c8ee12a379
akop <akopov.ab@gmail.com>
2020-02-26 00:01:01 +0300

    two les

--- src/main/java/org/example/untitled/GreetingController.java
+++ src/main/java/org/example/untitled/GreetingController.java
@@ -1,14 +1,20 @@
 package org.example.untitled;
 
+import org.example.untitled.domain.Message.Message;
+import org.example.untitled.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo MessageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name
@@ -18,9 +24,33 @@ public class GreetingController {
     }
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hellow lol main");
+        Iterable<org.example.untitled.domain.Message.Message> messages = MessageRepo.findAll();
+        model.put("messages", messages);
                 return "main";
-
-
     }
-}
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+
+        org.example.untitled.domain.Message.Message message = new Message(text, tag);
+
+        MessageRepo.save(message);
+        Iterable<org.example.untitled.domain.Message.Message> messages = MessageRepo.findAll();
+        model.put("messages", messages);
+        return  "main" ;
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<org.example.untitled.domain.Message.Message> messages;
+        if (filter !=null && !filter.isEmpty()) {
+            messages = MessageRepo.findByTag(filter);
+        } else{
+            messages = MessageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+        return  "main" ;
+    }
+    }
+
---
+++ src/main/java/org/example/untitled/domain/Message.java
@@ -0,0 +1,44 @@
+package org.example.untitled.domain.Message;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Message {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message() {
+    }
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getID() {
+        return id;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+    public String getTag() {
+        return tag;
+    }
+
+}
